ed9559faa066e0bce427ca8e2c71f3711a437230
Eliot Glairon <ejg_dnd@yahoo.com>
2013-11-04 08:10:59 -0800

    first commit test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WarlocksRemake::Application.config.secret_key_base = '375d692ef7d6f808e9d4defb13b0316792a18a9844290ad8830230d199b43521cce2bd13bdd226def150a2ccfdd05d30412dd066b4b88186db396200c7a87dc5'
